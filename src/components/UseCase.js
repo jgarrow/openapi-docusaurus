e607617a2e249e29be24e548c85a6eca8f2d378f
Janessa Garrow <janessa.garrow@mx.com>
2023-03-10 09:52:49 -0500

    Fix spacing on use case cards

--- src/components/UseCase.js
+++ src/components/UseCase.js
@@ -11,13 +11,13 @@ import { ChevronRight } from 'lucide-react';
 
 export function UseCase({ title, Svg, slug, children }) {
   return (
-    <div className="flex flex-col justify-between mb-">
+    <div className="flex flex-col">
       <div className="w-16 h-16">
         <Svg className="w-full h-full" />
       </div>
       <div className="font-bold text-2xl my-4">{title}</div>
       <p className="relative pl-4 mb-7 before:w-[4px] before:rounded-[4px] before:absolute before:content-[''] before:left-0 before:top-0 before:bottom-0 before:bg-gradient-to-t from-[#6A41BD] via-[#2F73DA_50.11%] to-[#6BCDDB]">{children}</p>
-      <Link to={slug} className="flex items-center">Read more <ChevronRight className="ml-3"/></Link>
+      <Link to={slug} className="flex items-center mt-auto">Read more <ChevronRight className="ml-3"/></Link>
     </div>
   )
 }
